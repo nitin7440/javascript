93d2c031448dba758faf62487a61151bbe23fde8
Nitin Prajapati <139560867+nitin7440@users.noreply.github.com>
2025-03-01 11:40:03 +0000

    day 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144503
+let accountEmail = "Nitin Prajapati"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "np@gmail.com"
+accountPassword = "21212121"
+accountCity = "mandideep"
+
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 )  => we are using node.js, not browser
+
+// documentation for Javascript tc39 and MDN;
+
+let name = "Nitin"  // String
+let age = 18    //Number 
+let isLoggedIn = false
+
+// number range => 2 to power 53
+// bigint 
+// string => "" 
+// boolean => true / false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+//object
+
+console.log(typeof null);
+
