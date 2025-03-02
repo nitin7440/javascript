22c2ef5c681bc4a810266befd14e43dbe0892232
Nitin Prajapati <139560867+nitin7440@users.noreply.github.com>
2025-03-02 09:21:53 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; , 0 => false
 // "" => false
@@ -24,8 +24,26 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// *************************************** Operations ********************************************
+
+let str1 = "hello"
+let str2 = "Nitin"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+
+
+
+
 
 
 
